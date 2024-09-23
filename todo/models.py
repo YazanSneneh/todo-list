8d2aa6b966cf7d2457ed0984bcf0a6a2a53422b5
Yazan Abu Sneneh <yaz.sneneh@gmail.com>
2024-09-23 06:07:31 +0300

    Implement authentication

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
-from todo.choices import TodoStatus, TodoPiriority
+from django.conf import settings
+from todo.choices import TodoStatus, TodoPriority
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.CharField(max_length=20, choices=TodoStatus.choices, default=TodoStatus.PENDING)
-    priority = models.CharField(max_length=20, choices=TodoPiriority.choices, default=TodoPiriority.Unknown)
+    priority = models.CharField(max_length=20, choices=TodoPriority.choices, default=TodoPriority.MEDIUM)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- todo/serializers.py
+++ todo/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    description = serializers.CharField()
-    status = serializers.CharField()
-    priority = serializers.CharField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
+class TodoSerializer(serializers.ModelSerializer):
+    status_display = serializers.SerializerMethodField(method_name="get_status_display")
+    priority_display = serializers.SerializerMethodField(method_name="get_priority_display")
+
+    class Meta:
+        model = Todo
+        fields = [ "title", "description", "status_display", "priority_display", "due_date"]
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
+
+    def get_priority_display(self, obj):
+        return obj.get_priority_display()
